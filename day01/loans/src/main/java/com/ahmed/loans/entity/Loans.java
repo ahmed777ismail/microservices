34b7a214ced3b7d91f403c152216d40ecba12b8c
ahmed777ismail <ahmed1kaballo@gmail.com>
2026-04-24 10:47:41 +0300

    Implement loans microservice with CRUD operations, error handling, and database configuration

--- day01/loans/src/main/java/com/ahmed/loans/entity/Loans.java
+++ day01/loans/src/main/java/com/ahmed/loans/entity/Loans.java
@@ -1,4 +1,25 @@
 package com.ahmed.loans.entity;
+import jakarta.persistence.*;
+import lombok.*;
 
-public class Loans {
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Loans extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanId;
+
+    private String mobileNumber;
+
+    private String loanNumber;
+
+    private String loanType;
+
+    private int totalLoan;
+
+    private int amountPaid;
+
+    private int outstandingAmount;
+
 }
--- day01/loans/src/main/java/com/ahmed/loans/exception/LoanAlreadyExistsException.java
+++ day01/loans/src/main/java/com/ahmed/loans/exception/LoanAlreadyExistsException.java
@@ -1,4 +1,13 @@
 package com.ahmed.loans.exception;
 
-public class LoanAlreadyExistsException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class LoanAlreadyExistsException extends RuntimeException {
+
+    public LoanAlreadyExistsException(String message){
+        super(message);
+    }
+
 }
