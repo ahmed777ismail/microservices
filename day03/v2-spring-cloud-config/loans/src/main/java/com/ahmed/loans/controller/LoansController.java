2fcf081083c77d48bd2159545439f6dd394e6a54
ahmed777ismail <ahmed1kaballo@gmail.com>
2026-04-28 15:36:12 +0300

    feat: Add spring-cloud-starter-bus-amqp and spring-cloud-config-monitor dependencies; update Docker images and configurations

--- day03/v2-spring-cloud-config/loans/src/main/java/com/ahmed/loans/controller/LoansController.java
+++ day03/v2-spring-cloud-config/loans/src/main/java/com/ahmed/loans/controller/LoansController.java
@@ -38,7 +38,7 @@ public class LoansController {
         this.iLoansService = iLoansService;
     }
 
-    @Value("${build.version}")
+    @Value("${build.version:1.0.0}")
     private String buildVersion;
 
     @Autowired
