ca1ae95a20b2c6ddd59c9863a73c8705b5f43101
ahmed777ismail <ahmed1kaballo@gmail.com>
2026-05-07 13:58:50 +0300

    feat: Implement Customer Details DTO and related services
    - Added CustomerDetailsDto with fields for name, email, mobile number, accounts, loans, and cards.
    - Created LoansDto with validation for mobile number, loan number, loan type, total loan, amount paid, and outstanding amount.
    - Updated CustomerMapper to include mapping for CustomerDetailsDto.
    - Enhanced ICustomersService interface to include fetchCustomerDetails method.
    - Implemented CardsFeignClient and LoansFeignClient for external service calls to fetch card and loan details.
    - Developed CustomersServiceImpl to fetch customer details, including accounts, loans, and cards using Feign clients.
    - Configured Eureka server settings in application.yml for service discovery.
    - Added necessary dependencies for Eureka client in pom.xml for cards and loans services.
    - Created Eureka server project with basic setup and configuration.

--- day05/accounts/src/main/java/com/ahmed/accounts/service/impl/CustomersServiceImpl.java
+++ day05/accounts/src/main/java/com/ahmed/accounts/service/impl/CustomersServiceImpl.java
@@ -1,4 +1,55 @@
 package com.ahmed.accounts.service.impl;
 
-public class CustomersServiceImpl {
+import com.ahmed.accounts.dto.AccountsDto;
+import com.ahmed.accounts.dto.CardsDto;
+import com.ahmed.accounts.dto.CustomerDetailsDto;
+import com.ahmed.accounts.dto.LoansDto;
+import com.ahmed.accounts.entity.Accounts;
+import com.ahmed.accounts.entity.Customer;
+import com.ahmed.accounts.exception.ResourceNotFoundException;
+import com.ahmed.accounts.mapper.AccountsMapper;
+import com.ahmed.accounts.mapper.CustomerMapper;
+import com.ahmed.accounts.repository.AccountsRepository;
+import com.ahmed.accounts.repository.CustomerRepository;
+import com.ahmed.accounts.service.ICustomersService;
+import com.ahmed.accounts.service.client.CardsFeignClient;
+import com.ahmed.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+
+    }
 }
