cfb613615b644a60170bec65512a5770f871976a
ahmed777ismail <ahmed1kaballo@gmail.com>
2026-05-07 11:48:35 +0300

    feat: Initialize Loans microservice with CRUD operations
    - Added pom.xml for Maven dependencies and configuration.
    - Created LoansApplication as the main entry point.
    - Implemented AuditAwareImpl for auditing purposes.
    - Defined LoansConstants for static values used in the application.
    - Developed LoansController for handling loan-related REST API requests.
    - Created DTOs: ErrorResponseDto, LoansContactInfoDto, LoansDto, and ResponseDto for data transfer.
    - Established BaseEntity and Loans entity for JPA mapping.
    - Implemented GlobalExceptionHandler for centralized exception handling.
    - Added custom exceptions: LoanAlreadyExistsException and ResourceNotFoundException.
    - Developed LoansMapper for mapping between entity and DTO.
    - Created LoansRepository for database interactions.
    - Implemented ILoansService interface and LoansServiceImpl for business logic.
    - Configured application.yml for Spring Boot settings.
    - Added schema.sql for database table creation.
    - Included basic test class for application context loading.

---
+++ day05/accounts/src/main/java/com/ahmed/accounts/repository/AccountsRepository.java
@@ -0,0 +1,19 @@
+package com.ahmed.accounts.repository;
+
+import com.ahmed.accounts.entity.Accounts;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+    Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
+
+}
